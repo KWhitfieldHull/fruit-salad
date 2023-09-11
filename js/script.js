acd5b77ae07c20eb43aeae74297cf9eb6b085bfb
Kieran <kieranhull@hotmail.co.uk>
2023-09-11 12:07:25 +0100

    Ready to use APIs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
 const fruitForm = document.querySelector("#inputSection form")
+const fruitList = document.querySelector("#fruitSection ul")
+
+const addFruit = fruit => {
+    const li = document.createElement("li")
+    li.textContent = fruit
+    fruitList.appendChild(li)
+}
+
+const delFruit = () => {
+    document.querySelector("li:hover").remove()
+}
 
 
 
-console.log(fruitForm)
+fruitForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    addFruit(e.target.fruitInput.value)
+    e.target.fruitInput.value = ""
+
+})
+
+//on click, remove fruit and event listeners associated with it
+
+fruitList.addEventListener("mousedown", e => {
+
+    //call deletefruit func here
+    delFruit();
+})
+
+
+
+
