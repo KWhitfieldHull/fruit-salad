2cf8406432643d03f1f69027974d51f6dd9d23e1
Kieran <kieranhull@hotmail.co.uk>
2023-09-11 15:02:20 +0100

    added data fetching, deleting is wip

--- js/script.js
+++ js/script.js
@@ -1,14 +1,63 @@
 const fruitForm = document.querySelector("#inputSection form")
+
 const fruitList = document.querySelector("#fruitSection ul")
+
+const fruitNutrition = document.querySelector("#nutritionSection p")
+
+const fetchFruitData = fruit => {
+    fetch(`https://fruity-api.onrender.com/api/fruits/${fruit}`)
+        .then(resp => resp.json())
+        .then(data => addFruit(data))
+        .catch(e => console.error(e))
+
+}
+
+let totalCal = 0
+let totalCarbs = 0
+let totalProtein = 0
+let totalSugar = 0
+let totalFat = 0;
+
 
 const addFruit = fruit => {
     const li = document.createElement("li")
-    li.textContent = fruit
+    li.textContent = fruit.name
     fruitList.appendChild(li)
+
+calcNutrition(fruit, '+')
+
+    
+
 }
 
+const calcNutrition = (fruit, operator) => {
+
+    if (operator == '+') {
+        totalCal += fruit.nutritions.calories
+        totalCarbs += fruit.nutritions.carbohydrates
+        totalProtein += fruit.nutritions.protein
+        totalSugar += fruit.nutritions.sugar
+        totalFat += fruit.nutritions.fat
+    }
+    else {
+        totalCal -= fruit.nutritions.calories
+        totalCarbs -= fruit.nutritions.carbohydrates
+        totalProtein -= fruit.nutritions.protein
+        totalSugar -= fruit.nutritions.sugar
+        totalFat -= fruit.nutritions.fat
+    }
+
+
+    fruitNutrition.textContent = `Calories: ${totalCal}\nProtein: ${totalProtein}\nCarbohydrates: ${totalCarbs}\nSugar: ${totalSugar}\nFat: ${totalFat}\n`
+
+}
+
+
+
 const delFruit = () => {
+    calcNutrition("li:hover", '-')
     document.querySelector("li:hover").remove()
+    
 }
 
 
@@ -16,15 +65,13 @@ const delFruit = () => {
 fruitForm.addEventListener("submit", e => {
     e.preventDefault();
 
-    addFruit(e.target.fruitInput.value)
+    fetchFruitData(e.target.fruitInput.value)
     e.target.fruitInput.value = ""
-
 })
 
 //on click, remove fruit and event listeners associated with it
 
 fruitList.addEventListener("mousedown", e => {
-
     //call deletefruit func here
     delFruit();
 })
